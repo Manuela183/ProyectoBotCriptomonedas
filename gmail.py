911fab63448d05a803471dc0ea4b9b9c7ec4231d
JuanGiraldoB <jugiraldob@unal.edu.co>
2022-06-19 19:39:47 -0500

    Se importa archivo config

--- gmail.py
+++ gmail.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-import os.path, base64, json, pprint as pp, email
+import os.path, base64, config, json, pprint as pp, email
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
