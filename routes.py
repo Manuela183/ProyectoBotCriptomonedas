0c927fd5bb14094514fc7442b87ebb8c540a3d43
Manuela183 <mamarulandaa@unal.edu.co>
2022-06-19 20:21:12 -0500

    Se agrega registro usuario

--- routes.py
+++ routes.py
@@ -5,3 +5,7 @@ from app import app
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/registroUsuario')
+def registro():
+    return render_template('registroUsuario.html')
