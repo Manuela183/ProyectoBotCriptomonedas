5f13e0fbf1b0a8c4d32f49751e911df67dae093b
JuanGiraldoB <jugiraldob@unal.edu.co>
2022-06-22 20:55:48 -0500

    Se agrega funcionalidad de encriptacion

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import bcrypt
 from flask import Flask
 import secrets
+from flask_bcrypt import Bcrypt
+
 
 app = Flask(__name__)
-
 secret = secrets.token_urlsafe(32)
-
 app.secret_key = secret
+bcrypt = Bcrypt(app)
 
 from routes import *
