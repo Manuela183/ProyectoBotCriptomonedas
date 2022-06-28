688a841ade51247fe134c4b45b934a842f494c73
JuanGiraldoB <jugiraldob@unal.edu.co>
2022-06-28 10:49:48 -0500

    .

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 secret = secrets.token_urlsafe(32)
 app.secret_key = secret
+app.config['SESSION_COOKIE_NAME'] = "my_session"
 bcrypt = Bcrypt(app)
 
 from routes import *
