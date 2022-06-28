6187f04baafc5de37d3a04e569cb17df589d5ded
JuanGiraldoB <jugiraldob@unal.edu.co>
2022-06-28 11:02:51 -0500

    cambio en secret key

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
-secret = secrets.token_urlsafe(32)
+secret = 'jafnjfsandkjfnasdfkjld1235fa1sd5f1asd3f21561312'
 app.secret_key = secret
 app.config['SESSION_COOKIE_NAME'] = "my_session"
 bcrypt = Bcrypt(app)
