ac5c07eee585ff3520f1c4cd42db65ca353cac3c
DanielDiazUnal <ddiazmo@unal.edu.co>
2022-06-28 21:46:36 -0500

    Se termina de completar el editar y se hacen pequeños arreglos

--- routes.py
+++ routes.py
@@ -9,6 +9,7 @@ import psycopg2.extras
 @app.route('/')
 @app.route('/index')
 def index():
+
     return render_template('index.html')
 
 
@@ -20,9 +21,16 @@ def report():
         if 'generar' in request.form:
 
             if 'role' in session and session['role'] == 'administrador':
-                return redirect(url_for('download_users'))
+                report_type_pdf = request.form["report_type_pdf"]
+                if report_type_pdf == 'users':
+
+                    return redirect(url_for('download_users'))
+
+                return redirect(url_for('download_orders'))
             else:
+
                 flash("Debe ser administrador para poder generar este reporte")
+
                 return render_template('report.html')
 
         report_type = request.form["report_type"]
@@ -57,7 +65,8 @@ def registro():
         try:
             conn = get_db_connection()
             cur = conn.cursor()
-            cur.execute("SELECT id FROM usuario WHERE nickname='{}'".format(nickname))
+            cur.execute(
+                "SELECT id FROM usuario WHERE nickname='{}'".format(nickname))
 
             exists = cur.fetchall()
 
@@ -118,7 +127,8 @@ def iniciar_sesion():
             flash("Ha iniciado sesión con exito")
             return redirect(url_for('index'))
         else:
-            flash("Ha ocurrido un error en el inicio de sesión, verifique nickname y contraseña")
+            flash(
+                "Ha ocurrido un error en el inicio de sesión, verifique nickname y contraseña")
 
     return render_template('inicio_sesion.html')
 
@@ -127,42 +137,68 @@ def iniciar_sesion():
 def cerrar_sesion():
     session.pop('id', None)
     session.pop('role', None)
-    
+
     flash('Se ha cerrado sesión con exito')
 
     return redirect(url_for('index'))
 
 
-@app.route('/editar_user/id', methods=["POST"])
-def editar(id):
+@app.route('/editar_user', methods=('GET', 'POST'))
+def editar():
 
     if request.method == 'POST':
         nickname = request.form['nickname']
+        password = request.form['password1']
         email = request.form['email']
         api = request.form['api']
         api_secret = request.form['api_secret']
-        
+        hash = bcrypt.generate_password_hash(password).decode('utf-8')
+        bandera = True
         try:
+                conn = get_db_connection()
+                cur = conn.cursor()
+
+                cur.execute('''
+            UPDATE usuario
+              SET nickname = %s,
+                password1 = %s,
+                email = %s,
+                api = %s,
+                api_secret = %s
+             WHERE id = %s
+            ''', (nickname, hash, email, api, api_secret, session['id']))
+
+        except (Exception, psycopg2.Error) as error:
+                bandera = False
+                flash("Error al intentar editar")
+                print("Error al obtener datos de la base de datos", error)
+                return render_template('editar_usuario.html')
+        finally:
+                conn.commit()
+                cur.close()
+                conn.close()
+
+        if bandera:
+            flash('Se a editado de forma exitosa')
+            return render_template('index.html')
+
+        flash('Error en la toma de datos')
+        return render_template('index.html')
+        
+    else:
+        if 'id' in session:
+            id = session['id']
             conn = get_db_connection()
             cur = conn.cursor()
-            cur.execute('''
-                UPDATE usuario
-                SET Nickname = %s,
-                    email = %s,
-                    api = %s,
-                    api_secret = %s
-                WHERE id = %s
-            ''', (nickname, email, api, api_secret, id))
-        except (Exception, psycopg2.Error) as error:
-            print("Error al obtener datos de la base de datos", error)
 
-        finally:
-            conn.commit()
-            cur.close()
-            conn.close()
-            
-        flash('Updated Successfully')
-        return redirect(url_for('index'))
+            cur.execute('''SELECT * FROM usuario WHERE id = {}'''.format(id))
+
+            usuario_edit = cur.fetchone()
+
+            return render_template('editar_usuario.html', usuario_edit=usuario_edit)
+        else:
+            flash("Deberá iniciar sesión primero")
+            return render_template('index.html')
 
 
 @app.route('/report/order/<symbol>')
@@ -184,22 +220,22 @@ def report_order(symbol):
 
         for order in orders:
             order['side'] = 'Venta' if order['side'] == 'SELL' else 'Compra'
-    
+
     except:
         flash("Error en credenciales de la api")
         bandera = False
-    
+
     finally:
         cur.close()
         conn.close()
-    
+
     if bandera:
         return render_template('report_order.html', orders=orders)
 
     return render_template('report.html')
 
 
-@app.route('/report/order/download')
+@app.route('/report/users/download')
 def download_users():
     try:
         conn = get_db_connection()
@@ -211,13 +247,13 @@ def download_users():
         pdf.add_page()
         pdf.set_font('Arial', 'B', 16)
 
-        pdf.multi_cell(w = 0, h= 7, txt='REPORTE DE USUARIOS', border=1, align='C', fill=0)
+        pdf.multi_cell(w= 0, h= 7, txt='REPORTE DE USUARIOS', border=1, align='C', fill=0)
         pdf.ln(0.25)
-        pdf.cell(w = 20, h= 8, txt='ID', border=1, align='C', fill=0)
+        pdf.cell(w= 20, h= 8, txt='ID', border=1, align='C', fill=0)
 
-        pdf.cell(w = 50, h= 8, txt='Nombre', border=1, align='C', fill=0)
+        pdf.cell(w= 50, h= 8, txt='Nombre', border=1, align='C', fill=0)
 
-        pdf.multi_cell(w = 0, h= 8, txt='Correo', border=1, align='C', fill=0)
+        pdf.multi_cell(w= 0, h= 8, txt='Correo', border=1, align='C', fill=0)
 
         for usuario in usuarios:
             pdf.cell(20, 8, str(usuario['id']), border=1)
@@ -227,19 +263,20 @@ def download_users():
         pdf.ln(10)
 
         pdf.set_font('Times', '', 10)
-       
+
         pdf.cell(0, 0.0, '- end of report -', align='C')
 
         return Response(pdf.output(dest='S').encode('latin-1'), mimetype='application/pdf', headers={'Content-Disposition': 'attachment;filename=reporte_usuarios.pdf'})
 
     except Exception as e:
-        print("Error en la creacion del pdf",e)
+        print("Error en la creacion del pdf", e)
 
     finally:
         cur.close()
         conn.close()
 
-app.route('/report/order/download')
+
+@app.route('/report/order/download')
 def download_orders():
     try:
         conn = get_db_connection()
@@ -251,23 +288,21 @@ def download_orders():
         pdf.add_page()
         pdf.set_font('Arial', 'B', 16)
 
-        pdf.multi_cell(w = 0, h= 7, txt='REPORTE DE ORDENES', border=1, align='C', fill=0)
+        pdf.multi_cell(w= 0, h= 7, txt='REPORTE DE ORDENES', border=1, align='C', fill=0)
         pdf.ln(0.25)
-        pdf.cell(w = 20, h= 8, txt='ID', border=1, align='C', fill=0)
+        pdf.cell(w= 12, h= 8, txt='ID', border=1, align='C', fill=0)
 
-        pdf.cell(w = 50, h= 8, txt='Toma de ganancias', border=1, align='C', fill=0)
-        pdf.cell(w = 50, h= 8, txt='Precio de entrada', border=1, align='C', fill=0)
-        pdf.cell(w = 50, h= 8, txt='Moneda', border=1, align='C', fill=0)
-        pdf.cell(w = 50, h= 8, txt='Detener las perdidas', border=1, align='C', fill=0)
+        pdf.cell(w= 50, h= 8, txt='Preciodeentrada', border=1, align='C', fill=0)
+        pdf.cell(w= 33, h= 8, txt='Moneda', border=1, align='C', fill=0)
+        pdf.cell(w= 57, h= 8, txt='Detener las perdidas', border=1, align='C', fill=0)
 
-        pdf.multi_cell(w = 0, h= 8, txt='Tipo', border=1, align='C', fill=0)
+        pdf.multi_cell(w= 0, h= 8, txt='Tipo', border=1, align='C', fill=0)
 
         for ordenes in ordenes:
-            pdf.cell(20, 8, str(ordenes['id']), border=1)
-            pdf.cell(50, 8, str(ordenes['tomar_ganancias']), border=1)
+            pdf.cell(12, 8, str(ordenes['id']), border=1)
             pdf.cell(50, 8, str(ordenes['precio_entrada']), border=1)
-            pdf.cell(50, 8, str(ordenes['moneda']), border=1)
-            pdf.cell(50, 8, str(ordenes['detener_perdidas']), border=1)
+            pdf.cell(33, 8, str(ordenes['moneda']), border=1)
+            pdf.cell(57, 8, str(ordenes['detener_perdidas']), border=1)
             pdf.multi_cell(0, 8, str(ordenes['tipo']), border=1)
 
         pdf.ln(10)
@@ -279,7 +314,7 @@ def download_orders():
         return Response(pdf.output(dest='S').encode('latin-1'), mimetype='application/pdf', headers={'Content-Disposition': 'attachment;filename=reporte_ordenes.pdf'})
 
     except Exception as e:
-        print("Error en la creacion del pdf",e)
+        print("Error en la creacion del pdf", e)
 
     finally:
         cur.close()
@@ -301,20 +336,20 @@ def report_coin_info(symbol):
         client = bc.init_client(api[0], api[1])
 
         coin_info = bc.get_symbol_info(symbol, client)
-    
+
     except:
         flash("Error en credenciales de la api")
         bandera = False
-    
+
     finally:
         cur.close()
         conn.close()
-    
+
     if bandera:
         return render_template('report_coin_info.html', coin_info=coin_info)
 
     return render_template('report.html')
-    
+
 
 @app.route('/trade', methods=('GET', 'POST'))
 def trade():
@@ -329,7 +364,7 @@ def trade():
             ammount = request.form['ammount']
             tp = int(request.form['tp']) / 100
             sl = int(request.form['sl']) / 100
-            
+
             try:
                 conn = get_db_connection()
                 cur = conn.cursor()
@@ -348,7 +383,7 @@ def trade():
                     type = 'Venta' if side == 'SELL' else 'Compra'
                     cur.execute('INSERT INTO trades (tipo, moneda, precio_entrada, tomar_ganancias, detener_perdidas)' 'VALUES (%s,%s,%s,%s,%s)',
                                 (type, symbol, entry_price, tp, sl))
-                    
+
                     conn.commit()
 
                 else:
@@ -360,11 +395,10 @@ def trade():
             finally:
                 cur.close()
                 conn.close()
-        
+
         else:
             flash('Debe iniciar sesión para poder crear ordenes')
 
         return render_template('trade.html')
 
     return render_template('trade.html')
-
